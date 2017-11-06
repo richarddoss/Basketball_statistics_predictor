8d8453063746267ab4f6019463fa119f50f78f97
richarddoss <richarddoss07195@gmail.com>
2017-11-06 13:37:08 -0600

    regular season

--- eloTrails.py
+++ eloTrails.py
@@ -20,7 +20,7 @@ def elo(r1,r2,k,s1,s2,Team1,Team2):
     r2_cap=r2+k*(s2-E2)
     return r1_cap,r2_cap
 
-dataFile=open('screenScraping/Totalteamstats.csv','r')
+dataFile=open('screenScraping/Total2teamstats.csv','r')
 reader=csv.reader(dataFile)
 j=0
 Teams=['ATL','BOS','BKN','CHA','CHI','CLE','DAL','DEL','DEN','DET','EST','FCB','GSW','HOU','IND','LAC','LAL','MAC','MEM','MIA','MIL','MIN','NOP','NYK','OKC','ORL','PHI','PHX','POR','RMD','SAC','SAS','SDS','SLA','TOR','USA','UTA','WAS','WLD','WST']
@@ -47,10 +47,10 @@ for row in reader:
             s2=1
         if (s1==1 and teams_rating[Team1]>teams_rating[Team2]) or(s2==1 and teams_rating[Team2]>teams_rating[Team1]):
             accuracy+=1
-        teams_rating[Team1],teams_rating[Team2]=elo(teams_rating[Team1],teams_rating[Team2],32,s1,s2,Team1,Team2)
+        teams_rating[Team1],teams_rating[Team2]=elo(teams_rating[Team1],teams_rating[Team2],20,s1,s2,Team1,Team2)
         print(Team1,"rating",teams_rating[Team1],Team2,"rating",teams_rating[Team2])
     j=j+1
-print(accuracy/2829)
-#for i in range(0,40):
-    #print(Teams[i],teams_rating[Teams[i]])
+print(accuracy)
+for i in range(0,40):
+    print(Teams[i],teams_rating[Teams[i]])
 #print(j)
--- screenScraping/totTeamstats.py
+++ screenScraping/totTeamstats.py
@@ -7,20 +7,20 @@ import time
 import csv
 Teams=['ATL','BOS','BKN','CHA','CHI','CLE','DAL','DEL','DET','GSW','HOU','IND','LAC','LAL','MEM','MIA','MIL','MIN','NOP','NYK','OKC','ORL','PHI','PHX','POR','SAC','SAS','TOR','UTA','WAS']
 driver= webdriver.Firefox()
-driver.get('http://stats.nba.com/search/team-game/#?sort=GAME_DATE&dir=-1&Season=2016-17&CF=PTS*gt*20')
+driver.get('http://stats.nba.com/search/team-game/#?CF=PTS*gt*20&sort=GAME_DATE&dir=-1&Season=2016-17&SeasonType=Regular%20Season,Pre%20Season')
 #runBtn=driver.find_element_by_xpath('/html/body/div[3]/div[2]/section/div/div[2]/div/div[2]/stats-run-it/button')
 #runBtn.click()
 time.sleep(30)
-matches=2829
+matches=2664
 count=50
-while count<=2829:
+while count<=matches:
     LoadMoreBtn=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[4]/nba-stat-table/div[2]/div/a')
     LoadMoreBtn.click()
     count=count+50
     time.sleep(10)
 p=0
-NoOfColumn=24
-csv_file = open('Totalteamstats.csv', 'w')
+NoOfColumn=25
+csv_file = open('RegularSeasonteamstats.csv', 'w')
 writer = csv.writer(csv_file)
 ColumnTitle=['TEAM', 'DATE', 'MATCHUP', 'W/L', 'MIN', 'PTS', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%', 'FTM', 'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', '+/-']
 writer.writerow(['TEAM', 'DATE', 'MATCHUP', 'W/L', 'MIN', 'PTS', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%', 'FTM', 'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', '+/-'])
@@ -29,7 +29,7 @@ for i in range(1,2829):
     for j in range(1,NoOfColumn):
         data=driver.find_element_by_xpath('/html/body/div[3]/div/div/div[4]/nba-stat-table/div[1]/div[1]/table/tbody/tr['+str(i)+']/td['+str(j)+']')
         #print(data.text)
-        teams_dict[ColumnTitle[j]] = data.text
+        teams_dict[ColumnTitle[j-1]] = data.text
     writer.writerow(teams_dict.values())
     print(i)
 csv_file.close()
