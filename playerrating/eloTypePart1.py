852ce9f97e95df3818d6e51550838cba130c57e9
richarddoss <richarddoss07195@gmail.com>
2018-01-27 22:21:47 -0600

    algo

--- playerrating/eloTypePart1.py
+++ playerrating/eloTypePart1.py
@@ -1,15 +1,33 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from collections import defaultdict
 import collections
 import time
 import re
 import csv
+def elo(fixedPlayer,playerRating,minutesPlayed,Team,OppTeam,TotTime,PTS):
+	m1=0
+	m2=0
+	for players in playerRating[Team]:
+		m1+=playerRating[Team][players]*minutesPlayed[Team][players]
+	for players in playerRating[OppTeam]:
+		m2+=playerRating[OppTeam][players]*minutesPlayed[OppTeam][players]
+	m1without=m1-playerRating[Team][fixedPlayer]*minutesPlayed[Team][fixedPlayer]
+	print("effective strength for {} player's team".format(fixedPlayer),m1without,"Effective strength for opp team",m2)
+	minWithoutPlayer=minutesPlayed[Team][fixedPlayer]*int(TotTime)
+	minWithPlayer=int(TotTime)-minWithoutPlayer
+	PtTeam=minWithoutPlayer*m1without+minWithPlayer*m1
+	PtOppTeam=int(TotTime)*m2
+	print("PointProduction by {}".format(Team),PtTeam)
+	print("PointProduction by {}".format(OppTeam),PtOppTeam)
+	expectedValue=PtTeam-PtOppTeam
+	print("update Value",PTS-expectedValue)
+	time.sleep(10)
 csv_file1 = open('TeamMatchups.csv', 'r')
 reader1 = csv.reader(csv_file1)
 i=0
 j=0
+minutesPlayed=defaultdict(dict)
+playerRating=defaultdict(dict)
+checklist=[]
 for row1 in reader1:
 	if i>0:
 		s=re.split('\W+',row1[2])
@@ -17,20 +35,39 @@ for row1 in reader1:
 		if len(s)==2:
 			Team1=s[0]
 			Team2=s[1]
+			c=Team1+Team2
 		else:
 			Team1=s[0]
 			Team2=s[2]
-		#print(row1[1],Team1,Team2,i)
-		csv_file2 = open('PlayerDetails.csv', 'r')
-		reader2 = csv.reader(csv_file2)
-		for row2 in reader2:
-			if j>0:
-				if (Team1==row2[1] or Team2==row2[1]) and row1[1]==row2[2]:
-					print(row2[0],"playername",row2[1],"team",row2[2],"date")
-					time.sleep(10)
-			j=j+1
-		csv_file2.close()
-			
+			c=Team2+Team1
+		if not(c in checklist):
+			csv_file2 = open('PlayerDetails.csv', 'r')
+			reader2 = csv.reader(csv_file2)
+			for row2 in reader2:
+				if j>0:
+					if (Team1==row2[1] or Team2==row2[1]) and row1[1]==row2[2]:
+						#print(row2[0],"playername",row2[1],"team",row2[2],"date")
+						#time.sleep(2)
+						if Team1==row2[1]:
+							minutesPlayed[Team1][row2[0]]=int(row2[5])/(int(row1[4]))
+							playerRating[Team1][row2[0]]=200
+						else:
+							minutesPlayed[Team2][row2[0]]=int(row2[5])/(int(row1[4]))
+							playerRating[Team2][row2[0]]=200
+				j=j+1
+			j=0
+			csv_file2.close()
+			checklist.append(c)
+			for fixedPlayer in playerRating[Team1]:
+				elo(fixedPlayer,playerRating,minutesPlayed,Team1,Team2,row1[4],int(row1[6]))
+			print("opp team update")
+			for fixedPlayer in playerRating[Team2]:
+				elo(fixedPlayer,playerRating,minutesPlayed,Team2,Team1,row1[4],-1*int(row1[6]))
+		else:
+			print("Trying to repeat")
+			checklist.remove(c)
+		
+	print("NEXT ROUND")
 	i=i+1
 csv_file1.close()
 csv_file2.close()
--- screenScraping/test.py
+++ screenScraping/test.py
@@ -6,19 +6,5 @@ import collections
 import time
 import csv
 Teams=['ATL','BOS','BKN','CHA','CHI','CLE','DAL','DEL','DET','GSW','HOU','IND','LAC','LAL','MEM','MIA','MIL','MIN','NOP','NYK','OKC','ORL','PHI','PHX','POR','SAC','SAS','TOR','UTA','WAS']
-driver= webdriver.Firefox()
-driver.get('http://stats.nba.com/search/team-game/#?CF=PTS*gt*20&sort=GAME_DATE&dir=1&Season=2016-17&GB=Y')
-time.sleep(30)
-filtersTab=driver.find_element_by_xpath('/html/body/div[3]/div[2]/section/div/div[2]/div/div[2]/button')
-filtersTab.click()
-teamTab=driver.find_element_by_xpath('/html/body/div[3]/div[2]/section/div/div[3]/div/div/form/div/section[2]/div/div[3]/div/div[4]')
-teamTab.click()
-selectTeam=driver.find_element_by_xpath('/html/body/div[3]/div[2]/section/div/div[3]/div/div/form/div/section[2]/div/div[2]/ul/li['+str(5)+']')
-selectTeam.click()
-runBtn=driver.find_element_by_xpath('/html/body/div[3]/div[2]/section/div/div[2]/div/div[2]/stats-run-it/button')
-runBtn.click()
-time.sleep(20)
-for i in range(1,4):
-    LoadMoreBtn = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[4]/nba-stat-table/div[2]/div/a')
-    print(LoadMoreBtn)
-    LoadMoreBtn.click()
+T.add('BOS')
+print(T)
