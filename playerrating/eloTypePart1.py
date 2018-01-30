5d5d45bc0512ed7dcd167d95d3bbe126873c30ce
richarddoss <richarddoss07195@gmail.com>
2018-01-29 20:07:13 -0600

    Elo part2

--- playerrating/eloTypePart1.py
+++ playerrating/eloTypePart1.py
@@ -11,23 +11,32 @@ def elo(fixedPlayer,playerRating,minutesPlayed,Team,OppTeam,TotTime,PTS):
 	for players in playerRating[OppTeam]:
 		m2+=playerRating[OppTeam][players]*minutesPlayed[OppTeam][players]
 	m1without=m1-playerRating[Team][fixedPlayer]*minutesPlayed[Team][fixedPlayer]
-	print("effective strength for {} player's team".format(fixedPlayer),m1without,"Effective strength for opp team",m2)
+	#print("effective strength for {} player's team".format(fixedPlayer),m1without,"Effective strength for opp team",m2)
 	minWithoutPlayer=minutesPlayed[Team][fixedPlayer]*int(TotTime)
 	minWithPlayer=int(TotTime)-minWithoutPlayer
 	PtTeam=minWithoutPlayer*m1without+minWithPlayer*m1
 	PtOppTeam=int(TotTime)*m2
-	print("PointProduction by {}".format(Team),PtTeam)
-	print("PointProduction by {}".format(OppTeam),PtOppTeam)
+	#print("PointProduction by {}".format(Team),PtTeam)
+	#print("PointProduction by {}".format(OppTeam),PtOppTeam)
 	expectedValue=PtTeam-PtOppTeam
-	print("update Value",PTS-expectedValue)
-	time.sleep(10)
+	#print("Actual",PTS,"Expected",expectedValue)
+	updateValue=(10*(PTS-expectedValue))/int(TotTime)
+	playerRating[Team][fixedPlayer]=playerRating[Team][fixedPlayer]+updateValue
+	#print("Updated player Rating for {} is".format(fixedPlayer),playerRating[Team][fixedPlayer])
+	#time.sleep(10)
+	return playerRating
+	
 csv_file1 = open('TeamMatchups.csv', 'r')
 reader1 = csv.reader(csv_file1)
 i=0
 j=0
 minutesPlayed=defaultdict(dict)
 playerRating=defaultdict(dict)
+duplicate=defaultdict(dict)
+tempTeam=defaultdict(dict)
+check=defaultdict(dict)
 checklist=[]
+flog=1
 for row1 in reader1:
 	if i>0:
 		s=re.split('\W+',row1[2])
@@ -50,21 +59,50 @@ for row1 in reader1:
 						#time.sleep(2)
 						if Team1==row2[1]:
 							minutesPlayed[Team1][row2[0]]=int(row2[5])/(int(row1[4]))
-							playerRating[Team1][row2[0]]=200
+							if check[row2[0]]=={}:
+								playerRating[Team1][row2[0]]=200
+								check[row2[0]]='Filled'
+								print("enter the dragon")
 						else:
 							minutesPlayed[Team2][row2[0]]=int(row2[5])/(int(row1[4]))
-							playerRating[Team2][row2[0]]=200
+							if check[row2[0]]=={}:
+								playerRating[Team2][row2[0]]=200
+								check[row2[0]]='Filled'
+								print("enter the dragon2")
 				j=j+1
 			j=0
 			csv_file2.close()
 			checklist.append(c)
+			print("%%%%%%%%%%%%%%%%%  Before Update %%%%%%%%%%%%%%%%")
+			m1=0
+			m2=0
+			for players in playerRating[Team1]:
+				m1+=playerRating[Team1][players]*minutesPlayed[Team1][players]
+			for players in playerRating[Team2]:
+				m2+=playerRating[Team2][players]*minutesPlayed[Team2][players]
+			print("Updated rating for {}".format(Team1),m1)
+			print("updated rating for {}".format(Team2),m2)
+			print(Team1,row1[3],Team2)
+			print("%%%%%%%%%%%% after update%%%%%%%%%%%%%%%")
+			duplicate=playerRating
 			for fixedPlayer in playerRating[Team1]:
-				elo(fixedPlayer,playerRating,minutesPlayed,Team1,Team2,row1[4],int(row1[6]))
-			print("opp team update")
+				playerRating=elo(fixedPlayer,playerRating,minutesPlayed,Team1,Team2,row1[4],int(row1[6]))
+			tempTeam=playerRating[Team1]
 			for fixedPlayer in playerRating[Team2]:
-				elo(fixedPlayer,playerRating,minutesPlayed,Team2,Team1,row1[4],-1*int(row1[6]))
+				playerRating=elo(fixedPlayer,duplicate,minutesPlayed,Team2,Team1,row1[4],-1*int(row1[6]))
+			playerRating[Team1]=tempTeam
+			m1=0
+			m2=0
+			for players in playerRating[Team1]:
+				m1+=playerRating[Team1][players]*minutesPlayed[Team1][players]
+			for players in playerRating[Team2]:
+				m2+=playerRating[Team2][players]*minutesPlayed[Team2][players]
+			print("Updated rating for {}".format(Team1),m1)
+			print("updated rating for {}".format(Team2),m2)
+			time.sleep(4)
 		else:
 			print("Trying to repeat")
+			#time.sleep(10)
 			checklist.remove(c)
 		
 	print("NEXT ROUND")
