40f1efc3e1498a5089001a76d1ed68e0fb5f6d41
richarddoss <richarddoss07195@gmail.com>
2018-05-04 14:05:54 -0500

    added season 2015-16

--- playerrating/playerGeneration.py
+++ playerrating/playerGeneration.py
@@ -6,14 +6,14 @@ import collections
 import time
 import csv
 driver= webdriver.Firefox()
-driver.get('http://stats.nba.com/search/player-game/#?Season=2015-16&SeasonType=Regular%20Season&sort=GAME_DATE&dir=-1&DateFrom=10%2F27%2F2015&DateTo=11%2F30%2F2015')
+driver.get('http://stats.nba.com/search/player-game/#?Season=2015-16&SeasonType=Regular%20Season&sort=GAME_DATE&dir=-1&DateFrom=04%2F01%2F2016&DateTo=04%2F13%2F2016')
 time.sleep(10)
 crossBtn=driver.find_element_by_xpath('/html/body/div[3]/div[2]/section/div/div[2]/div/div[1]/div/div/table/tbody/tr/td[4]/button')
 crossBtn.click()
 runBtn=driver.find_element_by_xpath('/html/body/div[3]/div[2]/section/div/div[2]/div/div[2]/stats-run-it/button')
 runBtn.click()
 time.sleep(30)
-totalGames = "5658"
+totalGames = "2123"
 print(totalGames)
 numberClicks=int(int(totalGames)/50)
 if int(totalGames)%50==0:
@@ -21,9 +21,9 @@ if int(totalGames)%50==0:
 for i in range(0,numberClicks):
         LoadMoreBtn = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[4]/nba-stat-table/div[2]/div/a')
         LoadMoreBtn.click()
-csv_file = open('PlayerDetails2015-2016.csv', 'w')
+csv_file = open('PlayerDetails2015-2016.csv', 'a')
 writer = csv.writer(csv_file)
-writer.writerow(['PLAYER','TEAM', 'DATE', 'MATCHUP', 'W/L', 'MIN', 'PTS', '+/-'])
+#writer.writerow(['PLAYER','TEAM', 'DATE', 'MATCHUP', 'W/L', 'MIN', 'PTS', '+/-'])
 for row in range(1,int(totalGames)+1):
 	teams_dict = collections.OrderedDict()
 	teams_dict['PLAYER']=driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[4]/nba-stat-table/div[1]/div[1]/table/tbody/tr['+str(row)+']/td[1]/a').text
