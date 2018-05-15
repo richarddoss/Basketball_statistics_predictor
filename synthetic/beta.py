3226e46bca6aef9f5faa8be50a3696e968377e1d
richarddoss <richarddoss07195@gmail.com>
2018-05-14 20:32:49 -0500

    beta distribution

--- synthetic/beta.py
+++ synthetic/beta.py
@@ -3,11 +3,11 @@ from scipy.stats import beta
 import matplotlib.pyplot as plt
 a=3
 b=3
-dist = beta(70, 70)
+dist = beta(11.02,11.02)
 x = np.linspace(0, 1, 1002)[1:-1]
-x1=x*600+900
+x1=x*48
 plt.plot(x1, dist.pdf(x))
-plt.xlabel("Player's true strength")
+plt.xlabel("Time spent on the court")
 plt.ylabel("Probability density")
-plt.title("Distribution of the strengths of professional basketball players")
+plt.title("Distribution of the time played by professional basketball players")
 plt.show()
